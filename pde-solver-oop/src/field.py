dfb2ba1c38c583682795bf3195fa65213e2c791b
hyh101923 <2803648334@qq.com>
2025-07-11 11:56:23 +0800

    Delete pde-solver-oop directory

--- pde-solver-oop/src/field.py
+++
@@ -1,8 +0,0 @@
-from .mesh import Mesh
-
-class Field(Mesh):
-    def __init__(self, start: float, h: float, N: int):
-        super().__init__(start, h, N)
-
-    def __str__(self) -> str:
-        pass
--- pde-solver-oop/src/mesh.py
+++
@@ -1,6 +0,0 @@
-class Mesh:
-    def __init__(self, start: float, h: float, N: int):
-        pass
-
-    def __str__(self) -> str:
-        pass
